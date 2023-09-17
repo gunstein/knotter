de255e4a9453befd03c60ed04488b3745dbf44c6
Gunstein Vatnar <gunstein.vatnar@gmail.com>
2023-09-17 15:32:55 +0200

    Some refactoring before doing big changes.

---
+++ src/globe.rs
@@ -0,0 +1,52 @@
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+pub struct GlobePlugin;
+
+impl Plugin for GlobePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_globe)
+            .insert_resource(GlobePos(Vec3::new(0.0, 0.0, 0.0)))
+            .insert_resource(GlobeRadius(1.0))
+            .register_type::<Globe>();
+    }
+}
+
+#[derive(Component, Default, Reflect)]
+#[reflect(Component)]
+pub struct Globe; 
+
+#[derive(Resource)]
+pub struct GlobePos(pub Vec3);
+
+#[derive(Resource)]
+pub struct GlobeRadius(pub f32);
+
+fn spawn_globe(mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    globe_pos: Res<GlobePos>,
+    globe_radius: Res<GlobeRadius>,) {
+
+    commands.spawn(
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: globe_radius.0,
+                ..default()
+            })),
+            material: materials.add(Color::BLACK.into()),
+            ..default()
+        }
+        )
+        .insert((TransformBundle::from(Transform::from_translation(globe_pos.0)),
+        RigidBody::Fixed,
+        Restitution {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Min,
+        },
+        Collider::ball(globe_radius.0),
+        Friction::coefficient(0.0),
+        Globe
+        )
+    );
+}
---
+++ src/query_server.rs
@@ -0,0 +1,43 @@
+use bevy::{log::LogPlugin, prelude::*};
+use bevy_mod_reqwest::*;
+
+pub struct QueryServerPlugin;
+
+impl Plugin for QueryServerPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .add_plugins(ReqwestPlugin)
+        .add_plugins(LogPlugin::default())
+        .add_systems(Update, send_requests)
+        .add_systems(Update, handle_responses)
+        .insert_resource(ReqTimer(Timer::new(
+            std::time::Duration::from_secs(1),
+            TimerMode::Repeating,
+        )));
+    }
+}
+
+#[derive(Resource)]
+struct ReqTimer(pub Timer);
+
+fn send_requests(mut commands: Commands, time: Res<Time>, mut timer: ResMut<ReqTimer>) {
+    timer.0.tick(time.delta());
+
+    if timer.0.just_finished() {
+        if let Ok(url) = "https://www.boredapi.com/api/activity".try_into() {
+            let req = reqwest::Request::new(reqwest::Method::GET, url);
+            let req = ReqwestRequest::new(req);
+            commands.spawn(req);
+        }
+    }
+}
+
+fn handle_responses(mut commands: Commands, results: Query<(Entity, &ReqwestBytesResult)>) {
+    for (e, res) in results.iter() {
+        let string = res.as_str().unwrap();
+        bevy::log::info!("{string}");
+
+        // Done with this entity
+        commands.entity(e).despawn_recursive();
+    }
+}
