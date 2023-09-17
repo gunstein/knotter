69d1249133e4cbdac9fc2db7f54d9775e71c06bb
Gunstein Vatnar <gunstein.vatnar@gmail.com>
2023-09-17 18:27:08 +0200

    Adding mocking_server.js.

--- mock_server.js
+++ mock_server.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/api/resource', (req, res) => {
-  res.json({ message: 'This is mock data!' });
+  res.json({ message: 'This is mock resource data!' });
 });
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'This is mock test data!' });
+  });
 
 app.listen(port, () => {
   console.log(`Mock server running at http://localhost:${port}`);
--- src/query_server.rs
+++ src/query_server.rs
@@ -8,9 +8,15 @@ impl Plugin for QueryServerPlugin {
         app
         .add_plugins(ReqwestPlugin)
         //.add_plugins(LogPlugin::default())
-        .add_systems(Update, send_requests)
-        .add_systems(Update, handle_responses)
+        .add_systems(Update, send_transactions_requests)
+        .add_systems(Update, handle_transactions_responses)
+        .add_systems(Update, send_test_requests)
+        .add_systems(Update, handle_test_responses)
         .insert_resource(ReqTimer(Timer::new(
+            std::time::Duration::from_secs(2),//Check if server has new data every 2 seconds
+            TimerMode::Repeating,
+        )))
+        .insert_resource(TestTimer(Timer::new(
             std::time::Duration::from_secs(2),//Check if server has new data every 2 seconds
             TimerMode::Repeating,
         )));
@@ -20,19 +26,50 @@ impl Plugin for QueryServerPlugin {
 #[derive(Resource)]
 struct ReqTimer(pub Timer);
 
-fn send_requests(mut commands: Commands, time: Res<Time>, mut timer: ResMut<ReqTimer>) {
+#[derive(Resource)]
+struct TestTimer(pub Timer);
+
+#[derive(Component)]
+pub struct TransactionsQuery;
+
+#[derive(Component)]
+pub struct TestQuery;
+
+fn send_transactions_requests(mut commands: Commands, time: Res<Time>, mut timer: ResMut<ReqTimer>) {
     timer.0.tick(time.delta());
 
     if timer.0.just_finished() {
         if let Ok(url) = "http://localhost:3000/api/resource".try_into() {
             let req = reqwest::Request::new(reqwest::Method::GET, url);
             let req = ReqwestRequest::new(req);
-            commands.spawn(req);
+            commands.spawn(req).insert(TransactionsQuery);
         }
     }
 }
 
-fn handle_responses(mut commands: Commands, results: Query<(Entity, &ReqwestBytesResult)>) {
+fn send_test_requests(mut commands: Commands, time: Res<Time>, mut timer: ResMut<TestTimer>) {
+    timer.0.tick(time.delta());
+
+    if timer.0.just_finished() {
+        if let Ok(url) = "http://localhost:3000/api/test".try_into() {
+            let req = reqwest::Request::new(reqwest::Method::GET, url);
+            let req = ReqwestRequest::new(req);
+            commands.spawn(req).insert(TestQuery);
+        }
+    }
+}
+
+fn handle_transactions_responses(mut commands: Commands, results: Query<(Entity, &ReqwestBytesResult), With<TransactionsQuery>>) {
+    for (e, res) in results.iter() {
+        let string = res.as_str().unwrap();
+        bevy::log::info!("{string}");
+
+        // Done with this entity
+        commands.entity(e).despawn_recursive();
+    }
+}
+
+fn handle_test_responses(mut commands: Commands, results: Query<(Entity, &ReqwestBytesResult), With<TestQuery>>) {
     for (e, res) in results.iter() {
         let string = res.as_str().unwrap();
         bevy::log::info!("{string}");
